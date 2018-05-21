f12f8c92a25fae7c49f57677711e33e5a9aeaa38
yangzongjun <zongjun.yang@longmaster.com.cn>
2018-05-21 14:48:08 +0800

    完成主界面模板绘制

--- public/js/main.js
+++ public/js/main.js
@@ -2,4 +2,20 @@ $(function () {
     var titleName = document.title;
    $("#page_name").html(titleName);
 
+   $(".display-person-info").on("mouseover", function () {
+       $(".child-table").css("display", "block");
+   });
+
+
 });
+
+//隐藏个人信息选择框
+function hide(object) {
+    object.css("display", "none");
+}
+
+
+//退出界面
+function exit(){
+
+}
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,23 +12,42 @@
     <link href="{{URL::asset('/css/main.css')}}" rel="stylesheet" type="text/css">
 </head>
 <body>
-    <div class="nav">
-        <table border="1">
-            <tr ><td id="page_name"></td></tr>
+<div class="nav">
+    <table>
+        <tr>
+            <td id="page_name"></td>
+        </tr>
+        <tr>
+            <td>会员卡佣金</td>
+        </tr>
+        <tr>
+            <td>会员卡使用记录</td>
+        </tr>
+        <tr>
+            <td>会员卡激活码管理</td>
+        </tr>
+        @if(Auth::user()->role_type === 1)
             <tr>
-                <td>会员卡佣金</td></tr>
-            <tr><td>会员卡使用记录</td></tr>
-            <tr><td>会员卡激活码管理</td></tr>
-            @if(Auth::user()->role_type === 1)
-                <tr><td>会员卡优惠券管理</td></tr>
-            @endif
-        </table>
+                <td>会员卡优惠券管理</td>
+            </tr>
+        @endif
+    </table>
+</div>
+<div class="person_info">
+    <div class="person_info_area">
+            <img class="display-person-info" src="{{URL::asset("/img/user.png")}}" height="20px">
+            <span class="display-person-info" style="position:  relative;top:  -3px;">杨宗俊</span>
+            <img class="display-person-info" src="{{URL::asset("/img/xiala.png")}}" height="20px">
+            <span style="position:  relative;top: -4px;color:  black;"> |</span>
+            <img class="exit" src="{{URL::asset("/img/close.png")}}" height="18px" onclick="exit()">
+            <span style="position:  relative;top: -2px;">退出</span>
+            <table class="child-table" onmouseleave="hide($(this))">
+                <tr><td>个人信息</td></tr>
+                <tr><td>修改密码</td></tr>
+            </table>
     </div>
-    <div class="">
-        <p>{{Auth::user()->name}}</p>
-        <p>退出</p>
-    </div>
-    @yield('content')
+</div>
+@yield('content')
 </body>
 </html>
 <script type="text/javascript" src="{{URL::asset("/js/jquery-1.11.2.min.js")}}"></script>
